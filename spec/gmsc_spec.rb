72a6c0d8bbda94c2104220b1f3a85f2a5d0a4d5b
Masaki Hara <ackie.h.gmai@gmail.com>
2023-12-15 12:52:37 +0900

    Support Array

--- spec/gmsc_spec.rb
+++ spec/gmsc_spec.rb
@@ -23,5 +23,26 @@ RSpec.describe GMSC do
       })
       expect(hash.to_json).to eq("{\"user-agent\":\"grpc-node/1.19.0 grpc-c/7.0.0 (linux; chttp2; gold)\",\"binary-data-bin\":\"AAEC6g==\"}")
     end
+
+    context "when metadata contains arrays" do
+      let(:metadata) {
+        {
+          "foo" => ["foo", "bar"],
+          "binfoo-bin" => ["\x00\x01\x02\xEA".b, "\x01\x02\x03\xFF".b],
+        }
+      }
+
+      it "converts array" do
+        hash = GMSC.safe_convert(metadata)
+        expect(hash).to(
+          eq(
+            {
+              "foo" => ["foo", "bar"],
+              "binfoo-bin" => ["AAEC6g==", "AQID/w=="],
+            }
+          )
+        )
+      end
+    end
   end
 end
