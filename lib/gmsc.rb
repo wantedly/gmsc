2f3258f8ac55ed138c9c329fad119e465fa87c53
Masaki Hara <ackie.h.gmai@gmail.com>
2023-12-15 15:03:23 +0900

    Support Array

--- lib/gmsc.rb
+++ lib/gmsc.rb
@@ -3,16 +3,16 @@ require "gmsc/version"
 
 module GMSC
   class << self
-    # @param [{ String => String }] metadata
-    # @return [{ String => String }]
+    # @param [{ String => String, Array<String> }] metadata
+    # @return [{ String => String, Array<String> }]
     def safe_convert(metadata)
       h = {}
       metadata.each do |k, v|
         if k.match?(/-bin$/)
           # If the value is binary, encode with Base64
-          h[k] = Base64.strict_encode64(v)
+          h[k] = v.is_a?(Array) ? v.map { |e| Base64.strict_encode64(e) } : Base64.strict_encode64(v)
         else
-          h[k] = v.encode(Encoding::UTF_8)
+          h[k] = v.is_a?(Array) ? v.map { |e| e.encode(Encoding::UTF_8) } : v.encode(Encoding::UTF_8)
         end
       end
       h
